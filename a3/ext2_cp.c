953b135f58828038235c68f9ca4b1877e5a84e38
Feilong Song <feilong.song@mail.utoronto.ca>
2018-07-24 15:52:23 -0400

    Handled some mistakes.

--- a3/ext2_cp.c
+++ a3/ext2_cp.c
@@ -26,19 +26,58 @@ int main(int argc, char **argv) {
     char * target_pathname = NULL;
     char * target_filename = NULL;
     split_last_part_of_path(argv[3], &target_pathname, &target_filename);
+
+    struct ext2_inode * find_result;
+    struct ext2_inode * tpath_inode;
+
     if (target_filename == NULL) {
+        // User give a path, no problem
         char * native_path;
         char * native_filename;
         split_last_part_of_path(argv[2], &native_path, &native_filename);
         free(native_path);
         target_filename = native_filename;
+    } else {
+        // User path does not end with '/', we want to know if the target filename exist, and if it is a directory
+        tpath_inode = get_inode_by_path(root_inode, target_pathname);
+        find_result = find_file(tpath_inode, target_filename);
+
+        if (find_result != NULL) {
+            // Exist file or directory
+            if (find_result->i_mode & EXT2_S_IFDIR) {
+                // This a target directory!!!!!!
+                free(target_filename);
+                free(target_pathname);
+
+                // Copy the original input since it's a directory
+                target_pathname = malloc(sizeof(char) * (strlen(argv[3]) + 1));
+                strncpy(target_pathname, argv[3], strlen(argv[3]));
+                target_pathname[strlen(argv[3])] = '\0';
+
+                // Copy source file name
+                char * native_path;
+                char * native_filename;
+                split_last_part_of_path(argv[2], &native_path, &native_filename);
+                free(native_path);
+                target_filename = native_filename;
+
+            } else {
+                // Exist link or file with same name
+                fprintf(stderr, "File already exist.\n");
+                exit(EEXIST);
+            }
+        }
+
     }
 
-
-    struct ext2_inode * tpath_inode = get_inode_by_path(root_inode, target_pathname);
+    tpath_inode = get_inode_by_path(root_inode, target_pathname);
+    if (!(tpath_inode->i_mode & EXT2_S_IFDIR)) {
+        printf("No such file or directory\n");
+        exit(ENOENT);
+    }
 
     // Check if already have a file with the same name
-    struct ext2_inode * find_result = find_file(tpath_inode, target_filename);
+    find_result = find_file(tpath_inode, target_filename);
     if (find_result != NULL) {
         fprintf(stderr, "File already exist.\n");
         exit(EEXIST);
